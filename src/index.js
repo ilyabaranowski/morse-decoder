8a6c5d3f60ae4563cd1dfccce66d3ec83659b9ad
iliuxa <iliuxa32@gmail.com>
2023-02-10 23:34:16 +0300

    morse task completed'
    gf
    f
    gf
    g
    dfg
    dddddddddddd
    '
    ;;:

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decodeMsg = '';
+    let startIndex = 0;
+    let endIndex = 9;
+    let decodeIndex = '';
+    while (endIndex <= expr.length - 1) {
+        let encodedSymb = expr.slice(startIndex, endIndex + 1);
+        let i;
+        for (i = 0; i < encodedSymb.length; i++) {
+            if (encodedSymb[i] !== '*') {
+                break;
+            }
+        }
+        if (i === encodedSymb.length) {
+            decodeMsg = decodeMsg + ' ';
+        } else  {
+            for (let j = 0; j < encodedSymb.length; j += 2) {
+                if (encodedSymb.slice(j, j + 2) === '10') {
+                    decodeIndex = decodeIndex + '.';
+                } else if (encodedSymb.slice(j, j + 2) === '11') {
+                    decodeIndex = decodeIndex +'-';
+                }
+            }
+            decodeMsg = decodeMsg + MORSE_TABLE[decodeIndex];
+        }
+        startIndex += 10;
+        endIndex += 10;
+        decodeIndex = '';
+    }
+    return decodeMsg;
 }
 
 module.exports = {
